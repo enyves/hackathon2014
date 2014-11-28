2005b094e056941af9e734c5c6f99fd9196dbe3d
unknown <tmp@EPHUBUDW2048.budapest.epam.com>
2014-11-28 20:48:01 +0100

    Dump

--- Marine.lua
+++ Marine.lua
@@ -11,24 +11,32 @@ function Marine:get_marine()
 end
 
 function Marine:select_mode()
-    print("select_mode called")
     return "advance"
 end
 
 function Marine:provide_steps(prev)
-    print("provide steps called " )
 
-    Marine:out()
+    local weapons, err = Game.Map:get_entities("w_")
+    print(dump(weapons))
 
-    return { { Command = "move", Path = Game.Map:get_move_path(self.marine_id, 10, 10) }, { Command = "done" } }
+    -- return { { Command = "move", Path = Game.Map:get_move_path(self.marine_id, 10, 10) }, { Command = "done" } }
+    return { Command = "done" }
 end
 
 function Marine:on_aiming(attack) end
 function Marine:on_dodging(attack) end
 function Marine:on_knockback(attack, entity) end
 
-function Marine:out()
-    local marine, err = Game.Map:get_entity(self.marine_id)
-    if (marine == nil) then print (err) end
-    for key,value in ipairs(marine) do print(key,value) end
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
 end
