38beaad2748cc2f33d933e7bb17fcd2ac49beb72
unknown <tmp@EPHUBUDW0239.budapest.epam.com>
2014-11-28 23:40:53 +0100

    Merged

--- Marine.lua
+++ Marine.lua
@@ -4,6 +4,7 @@ function Marine:initialize(player_index, marine_id, instance_index)
     self.player_index = player_index
     self.marine_id = marine_id
     self.instance_index = instance_index
+    self.target = nil
 end
 
 function Marine:get_marine()
@@ -11,23 +12,34 @@ function Marine:get_marine()
 end
 
 function Marine:select_mode()
+    --self.target = self:getClosest()
     return "advance"
 end
 
 function Marine:provide_steps(prev)
     if (prev) then return nil end
 
-    self:shortest_path_to_weapon()
+    print(dump(self:shortest_path_to_weapon()))
 
     -- return { { Command = "move", Path = Game.Map:get_move_path(self.marine_id, 10, 10) }, { Command = "done" } }
     return { Command = "done" }
+--[[=======
+    if (self.target ~= nil) then
+        print("moving!") 
+        local targetEntity = Game.Map:get_entity(self.target)
+        print(targetEntity.Bounds.X .. "," .. targetEntity.Bounds.Y)
+        local path = Game.Map:get_move_path(self.marine_id, targetEntity.Bounds.X, targetEntity.Bounds.Y);
+        print(dump(path))
+        return { { Command = "move", Path = path }, { Command = "done" } }
+    end
+    return { { Command = "done" } }
+    
+>>>>>>> Stashed changes--]]
 end
 
 function Marine:on_aiming(attack) end
 function Marine:on_dodging(attack) end
 function Marine:on_knockback(attack, entity) end
-
-
 
 function Marine:shortest_path_to_weapon()
     local weapons = Game.Map:get_entities("w_")
@@ -54,7 +66,34 @@ function Marine:shortest_path_to_weapon()
     -- print(dump(weapons[minInd]))
     
     return weapons[minInd].Bounds
+end
 
+function Marine:distance(entity2_id) 
+    local entity2 = Game.Map:get_entity(entity2_id)
+    if (entity2 == nil) then 
+        print ("Entity " .. entity2_id .. " not found!") 
+        return 9999 
+    end
+    local steps = Game.Map:get_move_path(self.marine_id, entity2.Bounds.X, entity2.Bounds.Y)
+    print(dump(steps));
+    if (#steps == 0) then return 9999 end
+    return #steps
+end
+
+function Marine:getClosest() 
+    local closest = nil
+    local closestDistance = 9999
+    for i = 1, #Marines do
+        if (Marines[i].marine_id ~= self.marine_id) then
+            local distance = self:distance(Marines[i].marine_id)
+            print("distance between " .. self.marine_id .. " and " .. Marines[i].marine_id .. " is " .. distance)
+            if (closestDistance > distance) then
+                closest = Marines[i].marine_id
+                closestDistance = distance
+            end            
+        end
+    end
+    return closest
 end
 
 function dump(o)
